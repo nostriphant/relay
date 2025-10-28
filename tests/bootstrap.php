54e7dc8d46025b13ab6401de01242c3eb9fb0b29
Rik Meijer <github@rikmeijer.nl>
2025-10-28 10:27:13 +0100

    cleanup afterwards

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,6 +11,21 @@ function files_directory() {
 function make_files_directory() {
     return \nostriphant\Relay\make_data_directory() && (is_dir(files_directory()) || mkdir(files_directory()));
 }
+
+function destroy_directories($path) {
+    foreach (glob($path . '/*') as $node) {
+        if (in_array(basename($node), ['.', '..'])) {
+            continue;
+        } elseif (is_file($node)) {
+            unlink($node);
+        } elseif (is_dir($node)) {
+            destroy_directories($node);
+            rmdir($node);
+        }
+    }
+}
+
 function destroy_files_directory() {
+    destroy_directories(files_directory());
     return (is_dir(files_directory()) === false || rmdir(files_directory())) && \nostriphant\Relay\destroy_data_directory();
 }
