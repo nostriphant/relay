1cc66b02dc837a1b8b728216d573b259398389b1
Rik Meijer <nostriphant@rikmeijer.nl>
2026-02-02 08:20:34 +0100

    refactor blossom

--- src/Blossom.php
+++ src/Blossom.php
@@ -7,38 +7,12 @@ readonly class Blossom {
     public function __construct(private string $path) {
        
     }
-    
-    private function file(string $hash) {
-        return new class($this->path . DIRECTORY_SEPARATOR . $hash) {
-
-            public function __construct(public string $path) {
-                
-            }
-
-            public function __invoke(): ?string {
-                if (func_num_args() === 0) {
-                    return file_get_contents($this->path);
-                }
-
-                list($event_id, $remote_file) = func_get_args();
-
-                $remote_handle = fopen($remote_file, 'r');
-                $local_handle = fopen($this->path, 'w');
-                while ($buffer = fread($remote_handle, 512)) {
-                    fwrite($local_handle, $buffer);
-                }
-                fclose($remote_handle);
-                fclose($local_handle);
-                return null;
-            }
-        };
-    }
 
     public function __invoke(callable $define) : void {
         $define(['HEAD', 'GET'], '/{hash:\w+}', function(string $hash) : array {
         
             $file_path = $this->path . DIRECTORY_SEPARATOR . $hash;
-            if (file_exists($file_path) === false) {
+            if (Blossom\File::exists($file_path) === false) {
                 return [
                     'code' => 404,
                     'headers' => [
@@ -48,7 +22,7 @@ readonly class Blossom {
                 ];
             }
             
-            $file = $this->file($hash);
+            $file = new Blossom\File($this->path . DIRECTORY_SEPARATOR . $hash);
             return [
                 'headers' => [
                     'Content-Type' => 'text/plain',
