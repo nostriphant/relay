068aab84fe75ba67faf9b779cc60080274610d71
Rik Meijer <nostriphant@rikmeijer.nl>
2026-02-02 10:02:58 +0100

    merge Regulare back into File class

--- src/Blossom/File.php
+++ src/Blossom/File.php
@@ -2,11 +2,26 @@
 
 namespace nostriphant\Relay\Blossom;
 
-abstract class File {
+readonly class File implements Endpoint {
+    
+    public function __construct(private string $path) {
+
+    }
+    
+    #[\Override]
+    public function __invoke(): array {
+        return [
+            'headers' => [
+                'Content-Type' => 'text/plain',
+                'Content-Length' => filesize($this->path)
+            ],
+            'body' => file_get_contents($this->path)
+        ];
+    }
     
     static function fromPath(string $path) : Endpoint {
         return match (file_exists($path)) {
-            true => new File\Regular($path),
+            true => new self($path),
             false => new File\Missing()
         };
     }
