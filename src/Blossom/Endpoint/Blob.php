c91f761439cca4aa860307da2bb2522f9f1f464b
Rik Meijer <nostriphant@rikmeijer.nl>
2026-02-03 09:57:15 +0100

    refactor blossom

--- src/Blossom/Endpoint/Blob.php
+++ src/Blossom/Endpoint/Blob.php
@@ -10,27 +10,28 @@ readonly class Blob implements Endpoint {
 
     }
     
+    static function makeEndpoint(string $path, callable $exists) {
+        return (new \nostriphant\Relay\Blossom\Blob($path))(
+            $exists, 
+            fn() => ['code' => 404]
+        );
+    }
+    
     #[\Override]
     public function __invoke(callable $define) : void {
-        $define('GET', '/{hash:\w+}', fn(array $attributes) => (new \nostriphant\Relay\Blossom\Blob($this->path . DIRECTORY_SEPARATOR . $attributes['hash']))(
-            fn(\nostriphant\Relay\Blossom\Blob $blob) => [
-                'headers' => [
-                    'Content-Type' => $blob->type,
-                    'Content-Length' => $blob->size
-                ],
-                'body' => $blob->contents
-            ], 
-            fn() => ['code' => 404]
-        ));
+        $define('GET', '/{hash:\w+}', fn(array $attributes) => self::makeEndpoint($this->path . DIRECTORY_SEPARATOR . $attributes['hash'], fn(\nostriphant\Relay\Blossom\Blob $blob) => [
+            'headers' => [
+                'Content-Type' => $blob->type,
+                'Content-Length' => $blob->size
+            ],
+            'body' => $blob->contents
+        ]));
         
-        $define('HEAD', '/{hash:\w+}', fn(array $attributes) => (new \nostriphant\Relay\Blossom\Blob($this->path . DIRECTORY_SEPARATOR . $attributes['hash']))(
-            fn(\nostriphant\Relay\Blossom\Blob $blob) => [
-                'headers' => [
-                    'Content-Type' => $blob->type,
-                    'Content-Length' => $blob->size
-                ]
-            ], 
-            fn() => ['code' => 404]
-        ));
+        $define('HEAD', '/{hash:\w+}', fn(array $attributes) => self::makeEndpoint($this->path . DIRECTORY_SEPARATOR . $attributes['hash'], fn(\nostriphant\Relay\Blossom\Blob $blob) => [
+            'headers' => [
+                'Content-Type' => $blob->type,
+                'Content-Length' => $blob->size
+            ]
+        ]));
     }
 }
