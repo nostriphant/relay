7794a84d21de097c37e429ab67663dafb8c91734
Rik Meijer <nostriphant@rikmeijer.nl>
2026-02-03 09:48:49 +0100

    refactor blossom

--- src/Blossom/Endpoint/GetBlob.php
+++ src/Blossom/Endpoint/GetBlob.php
@@ -12,17 +12,15 @@ readonly class GetBlob implements Endpoint {
     
     #[\Override]
     public function __invoke(callable $define) : void {
-        $define('GET', '/{hash:\w+}', function(array $attributes): array {
-            return (new \nostriphant\Relay\Blossom\Blob($this->path . DIRECTORY_SEPARATOR . $attributes['hash']))(
-                fn(\nostriphant\Relay\Blossom\Blob $blob) => [
-                    'headers' => [
-                        'Content-Type' => $blob->type,
-                        'Content-Length' => $blob->size
-                    ],
-                    'body' => $blob->contents
-                ], 
-                fn() => ['code' => 404]
-            );
-        });
+        $define('GET', '/{hash:\w+}', fn(array $attributes) => (new \nostriphant\Relay\Blossom\Blob($this->path . DIRECTORY_SEPARATOR . $attributes['hash']))(
+            fn(\nostriphant\Relay\Blossom\Blob $blob) => [
+                'headers' => [
+                    'Content-Type' => $blob->type,
+                    'Content-Length' => $blob->size
+                ],
+                'body' => $blob->contents
+            ], 
+            fn() => ['code' => 404]
+        ));
     }
 }
