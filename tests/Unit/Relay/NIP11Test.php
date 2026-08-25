da5d36a341ec6140856a41962df20292d5ae8e8c
Rik Meijer <nostriphant@rikmeijer.nl>
2026-08-25 14:52:53 +0200

    Change contact email in NIP11Test
    Updated the contact email in the Relay Information Document test.

--- tests/Unit/Relay/NIP11Test.php
+++ tests/Unit/Relay/NIP11Test.php
@@ -7,7 +7,7 @@ it('generates Relay Information Document', function () {
     $name = 'Transpher Relay';
     $description = 'Some interesting description goes here';
     $owner_pubkey = 'c0bb181bc39c4e59768805bbc5bdd34c508f14b01a298d63be4510d97417ce01';
-    $contact = 'transpher@nostriphant.dev';
+    $contact = 'nostriphant@rikmeijer.nl';
     $nips = [0,1,2];
     $software = 'MyRelaySoftware';
     $version = '1.0.1';
@@ -16,7 +16,7 @@ it('generates Relay Information Document', function () {
         "name" => 'Transpher Relay',
         "description" => 'Some interesting description goes here',
         "pubkey" => 'c0bb181bc39c4e59768805bbc5bdd34c508f14b01a298d63be4510d97417ce01',
-        "contact" => 'transpher@nostriphant.dev',
+        "contact" => 'nostriphant@rikmeijer.nl',
         "supported_nips" => [0,1,2],
         "software" => 'MyRelaySoftware',
         "version" => '1.0.1'
