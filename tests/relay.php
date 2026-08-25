43c599d00184e8a4fa573adcb429128ecc2d123f
Rik Meijer <nostriphant@rikmeijer.nl>
2026-08-25 14:53:46 +0200

    Change contact email in relay test
    Updated contact email for Transpher Relay.

--- tests/relay.php
+++ tests/relay.php
@@ -19,7 +19,7 @@ $relay = new \nostriphant\Relay\Relay(new \nostriphant\Relay\InformationDocument
     name: "Transpher Relay",
     description: "Some interesting description goes here",
     pubkey: (new \nostriphant\NIP19\Bech32((string) nostriphant\NIP19\Bech32::npub("c0bb181bc39c4e59768805bbc5bdd34c508f14b01a298d63be4510d97417ce01")))(),
-    contact: "transpher@nostriphant.dev",
+    contact: "nostriphant@rikmeijer.nl",
     supported_nips: [1, 2, 9, 11, 12, 13, 16, 20, 22, 33, 45],
     software: "https://github.com/nostriphant/relay",
     version: "2.2.0"
