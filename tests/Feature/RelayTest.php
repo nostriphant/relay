6b39737c76c2712a5e6273a3ecbf33eb4a24462e
Rik Meijer <nostriphant@rikmeijer.nl>
2026-08-25 14:53:23 +0200

    Update contact email in RelayTest.php

--- tests/Feature/RelayTest.php
+++ tests/Feature/RelayTest.php
@@ -8,7 +8,7 @@ it('boots a relay instance, which responds with an NIP-11 information document o
             'name' => 'Transpher Relay',
             'description' => 'Some interesting description goes here',
             'pubkey' => 'c0bb181bc39c4e59768805bbc5bdd34c508f14b01a298d63be4510d97417ce01',
-            'contact' =>'transpher@nostriphant.dev',
+            'contact' =>'nostriphant@rikmeijer.nl',
             'supported_nips' => [1, 2, 9, 11, 12, 13, 16, 20, 22, 33, 45],
             'software' => "https://github.com/nostriphant/relay",
             'version' => "2.2.0"
@@ -22,7 +22,7 @@ it('boots a relay instance, which responds with an NIP-11 information document o
             'name' => 'Transpher Relay',
             'description' => 'Some interesting description goes here',
             'pubkey' => 'c0bb181bc39c4e59768805bbc5bdd34c508f14b01a298d63be4510d97417ce01',
-            'contact' =>'transpher@nostriphant.dev',
+            'contact' =>'nostriphant@rikmeijer.nl',
             'supported_nips' => [1, 2, 9, 11, 12, 13, 16, 20, 22, 33, 45],
             'software' => "https://github.com/nostriphant/relay",
             'version' => "2.2.0"
